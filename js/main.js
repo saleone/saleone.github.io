131b951f4517f66ad0e5a93a1e355d703b49dc23
Saša Savić <sasa@savic.pw>
2017-01-03 22:21:46 +0100

    Add simple frontpage placeholder

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
+$(document).ready(function(){
+    $(window).resize(function(){
+        $('.mid-container').css({
+        position:'absolute',
+        left: ($(window).width()
+            - $('.mid-container').outerWidth())/2,
+        top: ($(window).height()
+            - $('.mid-container').outerHeight())/2
+        });
+    });
 
+    $('#blog-link').hover(
+        // hover in
+        function() {
+            $('#main-header').text('Comming Soon')
+        },
+        // hover out
+        function() {
+            $('#main-header').text('Saša Savić')
+        }
+    )
+
+    $(window).resize();
+
+});
